1c077458ab311172ae9ee74e202685dc85bed94e
joaquin <jecorrea@protonmail.com>
2021-09-27 15:55:01 -0400

    commented import

--- src/components/ProfileContainer.js
+++ src/components/ProfileContainer.js
@@ -1,4 +1,4 @@
-import Box from '@material-ui/core/Box';
+// import Box from '@material-ui/core/Box';
 import React, { Component } from "react";
 import Entry from "./Entry";
 import NewEntryForm from "./NewEntryForm";
